523e709945e850d338bd51f2f8235d6705763e34
mei0424 <18348889481@163.com>
2018-10-31 19:30:51 +0800

    登录页面渲染

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// @标识会自动锁定src文件夹
+import Login from '@/views/login'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [{
+    path: '/login',
+    component: Login
+  }]
 })
---
+++ src/views/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login-wrap">
+    <el-form class="login-form" label-position="top" label-width="80px">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-button" type="primary">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .login-wrap {
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .login-form {
+    background-color: #fff;
+    width: 400px;
+    padding: 30px;
+    border-radius: 5px;
+  }
+  .login-wrap .login-form .login-button {
+    width: 100%;
+  }
+</style>
